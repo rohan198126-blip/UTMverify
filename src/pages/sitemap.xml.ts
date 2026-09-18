8e31fd30222328eb326b95518f69f945f5671561
Divyansh <rohan198126@gmail.com>
2026-09-18 20:30:45 +0530

    fix: correct sitemap XML format

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -18,38 +18,24 @@ const PATHS = [
 ];
 
 export const GET: APIRoute = () => {
-  const urls: string[] = [];
+  const urlEntries: string[] = [];
 
   for (const p of PATHS) {
     for (const locale of SUPPORTED_LOCALES) {
       const locPath = p ? `${locale}/${p}` : `${locale}/`;
       const fullUrl = `${SITE_URL}/${locPath}`;
 
-      // Alternate hreflang tags for each language + x-default
-      const alternates = SUPPORTED_LOCALES.map((l) => {
-        const altPath = p ? `${l}/${p}` : `${l}/`;
-        return `    <xhtml:link rel="alternate" hreflang="${l}" href="${SITE_URL}/${altPath}" />`;
-      });
-      const defaultAlt = `    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}/en/${p ? p : ''}" />`;
-
-      urls.push(`  <url>
-    <loc>${fullUrl}</loc>
-${alternates.join('\n')}
-${defaultAlt}
-    <changefreq>weekly</changefreq>
-    <priority>${p === '' || p.startsWith('utm-') ? '0.9' : '0.7'}</priority>
-  </url>`);
+      urlEntries.push(`  <url>\n    <loc>${fullUrl}</loc>\n  </url>`);
     }
   }
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-${urls.join('\n')}
-</urlset>`;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urlEntries.join('\n')}\n</urlset>\n`;
 
   return new Response(xml, {
     headers: {
       'Content-Type': 'application/xml; charset=utf-8',
+      'X-Content-Type-Options': 'nosniff',
     },
   });
 };
+
